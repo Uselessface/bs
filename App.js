92dc85092c60cd72d41b92b0e10e7f4115b2e1c3
Uselessface <60576572+Uselessface@users.noreply.github.com>
2024-09-24 13:35:48 +0400

    changed  to expo camera

---
+++ App.js
@@ -0,0 +1,96 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {CameraView, Camera} from "expo-camera";
+import * as FileSystem from 'expo-file-system';
+import {StorageAccessFramework} from "expo-file-system";
+
+export default function App() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [dataArray, setDataArray] = useState([]);
+    const [file, setFile] = useState('');
+    const [path, setPath] = useState('')
+    const today = new Date();
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const {status} = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        };
+
+        getBarCodeScannerPermissions();
+    }, []);
+
+    useEffect(() => {
+        const getDirectory = async () =>{
+            const {directoryUri} = await StorageAccessFramework.requestDirectoryPermissionsAsync()
+            setPath(directoryUri)
+        }
+        getDirectory()
+    }, [])
+
+    useEffect(() => {
+        const saveFile = async () => {
+            try {
+                // Get the directory uri that was approved
+                let data = file
+                // Create file and pass it's SAF URI
+                await StorageAccessFramework.createFileAsync(
+                    path,
+                    "barcodes",
+                    "text/plain"
+                )
+                    .then(async (fileUri) => {
+                        // Save data to newly created file
+                        await FileSystem.writeAsStringAsync(fileUri, data, {
+                            encoding: FileSystem.EncodingType.UTF8
+                        })
+                    })
+                    .catch((e) => {
+                        console.log(e)
+                    })
+
+            } catch (err) {
+                console.warn(err)
+            }
+        }
+
+        const intervalID = setInterval(saveFile, 30000)
+
+        return () => clearInterval(intervalID)
+    }, [file])
+
+
+    const handleBarCodeScanned = ({type, data}) => {
+        setScanned(true);
+        setDataArray([...dataArray, `${today.toLocaleString()} : Штрихкод  ${data}`])
+        setFile(dataArray.join('; \n'))
+        setTimeout(setScanned, 250, false)
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <View style={styles.container}>
+            <CameraView
+                barcodeScannerSettings={{
+                    barcodeTypes: ["qr", "pdf417","ean13","ean8","codabar"],
+                }}
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+});
