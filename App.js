8fec32651d942e6c90a662269e3fc3e452fa6e45
Uselessface <60576572+Uselessface@users.noreply.github.com>
2024-09-27 15:07:07 +0400

    fetching агагfuck

--- App.js
+++ App.js
@@ -28,33 +28,33 @@ export default function App() {
     }, []);
 
 
-    useEffect(() => {
-        const saveFile = async () => {
-            try {
-                // Get the directory uri that was approved
-                let data = file
-                // Create file and pass it's SAF URI
-                await StorageAccessFramework.createFileAsync(
-                    path,
-                    "barcodes",
-                    "text/plain"
-                )
-                    .then(async (fileUri) => {
-                        // Save data to newly created file
-                        await FileSystem.writeAsStringAsync(fileUri, data, {
-                            encoding: FileSystem.EncodingType.UTF8
-                        })
-                    })
-                    .catch((e) => {
-                        console.log(e)
-                    })
-            } catch (err) {
-                console.warn(err)
-            }
-        }
-        const intervalID = setInterval(saveFile, 30000)
-        return () => clearInterval(intervalID)
-    }, [file]);
+    // useEffect(() => {
+    //     const saveFile = async () => {
+    //         try {
+    //             // Get the directory uri that was approved
+    //             let data = file
+    //             // Create file and pass it's SAF URI
+    //             await StorageAccessFramework.createFileAsync(
+    //                 path,
+    //                 "barcodes",
+    //                 "text/plain"
+    //             )
+    //                 .then(async (fileUri) => {
+    //                     // Save data to newly created file
+    //                     await FileSystem.writeAsStringAsync(fileUri, data, {
+    //                         encoding: FileSystem.EncodingType.UTF8
+    //                     })
+    //                 })
+    //                 .catch((e) => {
+    //                     console.log(e)
+    //                 })
+    //         } catch (err) {
+    //             console.warn(err)
+    //         }
+    //     }
+    //     const intervalID = setInterval(saveFile, 30000)
+    //     return () => clearInterval(intervalID)
+    // }, [file]);
     useEffect(() => {
         const uploadFile = async () => {
             try {
